bdce536f490b746c8ded020c0949f9d750da3225
Amkam <amaduskamara@gmail.com>
2023-01-21 14:12:57 +0000

    Create Header.jsx

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='flex justify-between px-4 pt-5'>
+      <h2>Dashboard</h2>
+      <h3>Welcome back, <span className="font-semibold">Amkam</span>.</h3>
+    </header>
+  )
+}
+
+export default Header
