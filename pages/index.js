fae8f3b094959643b7974f974b2f93eb3828b670
Amkam <amaduskamara@gmail.com>
2023-01-21 14:13:30 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import DashboardCards from "@/components/DashboardCards";
+import Header from "@/components/Header";
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -14,9 +11,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1 className='text-2xl text-teal-500'>Enterprise App Dashboard</h1>
+      <main className="bg-gray-100 min-h-screen">
+        <Header />
+        <DashboardCards />
       </main>
     </>
-  )
+  );
 }
