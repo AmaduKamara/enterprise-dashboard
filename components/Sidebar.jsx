1e4829bbc2541c13199e788dbfb4b91d16bb3052
Amkam <amaduskamara@gmail.com>
2023-01-22 12:38:52 +0000

    Update Sidebar.jsx

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -10,32 +10,32 @@ const Sidebar = ({ children }) => {
     <div className="flex">
       <aside className="fixed w-20 h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between">
         <div className="flex flex-col items-center">
-          <Link href="/" passHref>
+          <Link href="/">
             <div className="bg-teal-600 hover:bg-teal-800 text-white p-3 rounded-lg inline-block">
               <RxSketchLogo />
             </div>
           </Link>
           <span className="border-b-[1px] border-gray-200 w-full p-2"></span>
 
-          <Link href="/" passHref>
+          <Link href="/">
             <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block">
               <RxDashboard />
             </div>
           </Link>
 
-          <Link href="/customers" passHref>
+          <Link href="/customers">
             <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block">
               <RxPerson />
             </div>
           </Link>
 
-          <Link href="/orders" passHref>
+          <Link href="/orders">
             <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block">
               <HiOutlineShoppingBag />
             </div>
           </Link>
 
-          <Link href="/" passHref>
+          <Link href="/">
             <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block">
               <FiSettings />
             </div>
