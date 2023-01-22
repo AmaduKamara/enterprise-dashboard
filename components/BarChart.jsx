79978412590d084567b2e5dc216a859b4ceffa0a
Amkam <amaduskamara@gmail.com>
2023-01-22 12:38:45 +0000

    Create BarChart.jsx

---
+++ components/BarChart.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const BarChart = () => {
+  const [chartData, setChartData] = useState({
+    datasets: [],
+  });
+  const [chartOptions, setChartOptions] = useState({});
+
+  return (
+    <>
+      <div className="w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white shadow-md">
+        <Bar data={chartData} options={chartOptions} />
+      </div>
+    </>
+  );
+};
+
+export default BarChart;
