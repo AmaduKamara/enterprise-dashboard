6671e6eaf7c7530f9b1141142a6bfd5747453980
Amkam <amaduskamara@gmail.com>
2023-01-22 13:06:41 +0000

    Update BarChart.jsx

--- components/BarChart.jsx
+++ components/BarChart.jsx
@@ -25,9 +25,36 @@ const BarChart = () => {
   });
   const [chartOptions, setChartOptions] = useState({});
 
+  useEffect(() => {
+    setChartData({
+      labels: ['Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'],
+      datasets: [
+        {
+          label: 'Sales $',
+          data: [18127, 22201, 19490, 17938, 24182, 17842, 22475],
+          borderColor: 'rgb(53, 162, 235)',
+          backgroundColor: 'rgb(15, 196, 196, 0.624)',
+        },
+      ],
+    });
+    setChartOptions({
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Daily Revenue',
+        },
+      },
+      maintainAspectRatio: false,
+      responsive: true,
+    });
+  }, []);
+
   return (
     <>
-      <div className="w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white shadow-md">
+      <div className="w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white rounded-md">
         <Bar data={chartData} options={chartOptions} />
       </div>
     </>
